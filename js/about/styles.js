31df01d01cca9a97940f3ec4d98fed5018230237
Jon Kuperman <jon.kuperman@gmail.com>
2016-11-06 20:10:12 -0800

    clean up element styling

--- js/about/styles.js
+++ js/about/styles.js
@@ -4,17 +4,54 @@
 
 const React = require('react')
 const ImmutableComponent = require('../components/immutableComponent')
+const SwitchControl = require('../components/switchControl')
 
 // Stylesheets go here
+require('../../less/about/styles.less')
+require('../../less/button.less')
+require('../../less/switchControls.less')
+require('../../less/forms.less')
 
 class AboutStyle extends ImmutableComponent {
   render () {
-    return <div>
+    return <div className='wrapper'>
+      <h1 data-l10n-id='introTitle' />
+      <p data-l10n-id='intro' />
       <h1 className='typography' data-l10n-id='typography' />
       <h1 data-l10n-id='h1' />
       <h2 data-l10n-id='h2' />
       <h3 data-l10n-id='h3' />
       <h4 data-l10n-id='h4' />
+
+      <h2 data-l10n-id='forms' />
+      <div className='container'>
+        <input placeholder='Input box' className='form-control' type='text' />
+        <pre><code>
+          // require('less/forms.less'){'\n'}{'\n'}
+          &lt;input className='form-control' type='text' />
+        </code></pre>
+      </div>
+      <div className='container'>
+        <select className='form-control'>
+          <option>Select Box</option>
+          <option>Second Choice</option>
+          <option>Third Choice</option>
+        </select>
+        <pre><code>
+          &lt;select className='form-control'>{'\n'}
+            &lt;option>Select Box&lt;/option>{'\n'}
+            &lt;option>Second Choice&lt;/option>{'\n'}
+            &lt;option>Third Choice&lt;/option>{'\n'}
+          &lt;/select>
+        </code></pre>
+      </div>
+      <h2 data-l10n-id='buttons' />
+      <span data-l10n-id='cancel' className='browserButton whiteButton inlineButton' onClick={this.onRemoveBookmark} />
+      <span data-l10n-id='done' className='browserButton primaryButton' onClick={this.onRemoveBookmark} />
+
+      <h2 data-l10n-id='switches' />
+      <SwitchControl rightl10nId='onByDefault' checkedOn='true' />
+      <SwitchControl rightl10nId='offByDefault' checkedOn='false' />
     </div>
   }
 }
